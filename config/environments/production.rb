f76fba47f336b0cddbd466ccd7163b327577825e
Patrick Dietrich <github@pdietrich.net>
2009-03-14 21:19:15 +0100

    * fixed validation bug * switched on caching in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,15 +8,14 @@ config.cache_classes = false # true
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
-config.action_view.cache_template_loading            = true
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-config.action_mailer.raise_delivery_errors = true
+config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address => 'localhost',
